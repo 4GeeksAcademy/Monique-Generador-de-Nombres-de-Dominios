f6c2578e4699ee2280aef098a8653e0591fbf4d2
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2024-11-26 08:16:55 +0000

    generador de nombres de dominio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//write your code here
+
+console.log("Generador de Nombres de Dominios");
+
+let pronombres = ["el", "nuestro"];
+let adjetivos = ["genial", "grande"];
+let substantivos = ["corredor", "mapache"];
+let extesiones = [".com", ".net", ".us", ".io"];
+
+for (let i = 0; i < pronombres.length; i++) {
+  // console.log(pronombres[i]);
+  for (let j = 0; j < adjetivos.length; j++) {
+    // console.log(pronombre[i] + adjetivos[j]);
+    for (let l = 0; l < substantivos.length; l++) {
+      // console.log(pronombre[i] + adjetivos[j] + substantivos[l]);
+      for (let m = 0; m < extesiones.length; m++) {
+        // eslint-disable-next-line no-console
+        console.log(
+          pronombres[i] + adjetivos[j] + substantivos[l] + extesiones[m]
+        );
+      }
+    }
+  }
+}
